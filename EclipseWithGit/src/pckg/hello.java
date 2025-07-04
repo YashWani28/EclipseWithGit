50c024149fe22f6c4259dc06f8924b0db4595b48
Yash <proprogramming28@gmail.com>
2025-07-04 13:18:25 +0530

    added 3 lines

--- EclipseWithGit/src/pckg/hello.java
+++ EclipseWithGit/src/pckg/hello.java
@@ -5,6 +5,9 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		int i = 5;
+		int j = 6;
+		System.out.println(i*j);
 	}
 
 }
